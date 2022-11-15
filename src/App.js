f13f649ec07b5621bf27d7f76cf563c2f1aaf0db
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2022-11-16 01:27:09 +0400

    Update files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Nav from './components/Nav/Nav'
 import Footer from './components/Footer/Footer'
+import "./App.css";
 
 const App = () => {
-    return (
-        
-        <div>
-            <Nav />
-            <Footer />
-        </div>
-    )
+    return <div>
+        <Nav />
+        <Footer />
+    </div>
 }
 
 export default App
